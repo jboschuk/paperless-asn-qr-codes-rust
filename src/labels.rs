ab1fdacd6f5b6bd6680c3170744ff86f55edad16
Jürgen Boschuk <j.boschuk@gmail.com>
2025-05-27 23:30:26 +0200

    add avery L4731 (currently untested!)

--- src/labels.rs
+++ src/labels.rs
@@ -19,7 +19,7 @@ pub enum PaperSize {
 #[derive(Debug, clap::ValueEnum, Clone)]
 pub enum PredefinedFormat {
     Avery3657,
-    // Custom,
+    AveryL4731,
 }
 
 impl PredefinedFormat {
@@ -33,28 +33,14 @@ impl PredefinedFormat {
                 margin: (Mm(8.0), Mm(21.75)),
                 page_size: PaperSize::A4,
             }),
-            // PredefinedFormat::Custom => {
-            //     if let (Some(width), Some(height), Some(left_offset), Some(top_offset)) = (
-            //         self.label_width,
-            //         self.label_height,
-            //         self.left_offset,
-            //         self.top_offset,
-            //     ) {
-            //         Some(LabelInfo {
-            //             label_size: (width, height),
-            //             labels_horizontal: self.labels_horizontal.unwrap_or(1),
-            //             labels_vertical: self.labels_vertical.unwrap_or(1),
-            //             margin: (left_offset, top_offset),
-            //             gutter_size: (
-            //                 self.gutter_h.unwrap_or(Mm(0.0)),
-            //                 self.gutter_v.unwrap_or(Mm(0.0)),
-            //             ),
-            //             page_size: self.page_size.clone(),
-            //         })
-            //     } else {
-            //         None
-            //     }
-            // }
+            PredefinedFormat::AveryL4731 => Some(LabelInfo {
+                labels_horizontal: 7,
+                labels_vertical: 27,
+                label_size: (Mm(25.4), Mm(10.)),
+                gutter_size: (Mm(2.5), Mm(0.)),
+                margin: (Mm(9.0), Mm(13.5)),
+                page_size: PaperSize::A4
+            })
         }
     }
 }
